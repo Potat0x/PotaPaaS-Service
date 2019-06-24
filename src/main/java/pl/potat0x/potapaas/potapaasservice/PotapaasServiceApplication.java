7b92c0f2238deb10062d8a7f3f1123b17571ff6d
Potat0x <Potat0x@users.noreply.github.com>
2019-06-24 21:59:07 +0200

    Init

---
+++ src/main/java/pl/potat0x/potapaas/potapaasservice/PotapaasServiceApplication.java
@@ -0,0 +1,12 @@
+package pl.potat0x.potapaas.potapaasservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotapaasServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PotapaasServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/potat0x/potapaas/potapaasservice/PotapaasServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package pl.potat0x.potapaas.potapaasservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PotapaasServiceApplicationTests {
+    @Test
+    public void contextLoads() {
+    }
+}

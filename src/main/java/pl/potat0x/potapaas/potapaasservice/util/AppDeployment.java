92c212b9cc71e54d709b429c82666dc22870addb
Potat0x <Potat0x@users.noreply.github.com>
2019-06-29 20:23:50 +0200

    Add deployment from GitHub

---
+++ src/main/java/pl/potat0x/potapaas/potapaasservice/util/AppDeployment.java
@@ -0,0 +1,95 @@
+package pl.potat0x.potapaas.potapaasservice.util;
+
+import com.spotify.docker.client.messages.ContainerConfig;
+import com.spotify.docker.client.messages.HostConfig;
+import com.spotify.docker.client.messages.PortBinding;
+import io.vavr.control.Either;
+import io.vavr.control.Try;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import static io.vavr.API.*;
+
+final class AppDeployment {
+
+    public enum DeploymentType {
+        NODEJS("Node.js (NPM)");
+
+        public final String value;
+
+        DeploymentType(String value) {
+            this.value = value;
+        }
+    }
+
+    private final String githubRepoUrl;
+    private final String branchName;
+    private final DeploymentType deploymentType;
+    private final String potapaasAppId;
+    private String containerId;
+
+    private DockerContainerManager containerManager = new DockerContainerManager(PotapaasConfig.get("docker_api_uri"));
+
+    public AppDeployment(DeploymentType deploymentType, String githubRepoUrl, String branchName) {
+        this.githubRepoUrl = githubRepoUrl;
+        this.branchName = branchName;
+        this.deploymentType = deploymentType;
+        potapaasAppId = UUID.randomUUID().toString();
+    }
+
+    public Either<String, String> deployFromGithub() {
+        return cloneRepo().flatMap(this::buildImage).flatMap(
+                imageId -> runApp(imageId)
+                        .map(containerId -> this.containerId = containerId)
+                        .map(x -> potapaasAppId)
+        );
+    }
+
+    public Try<Boolean> killApp() {
+        return containerManager.killContainerIfRunning(containerId);
+    }
+
+    private Either<String, String> runApp(String imageId) {
+        Map<String, List<PortBinding>> stringListMap = new HashMap<>();
+        stringListMap.put("5000/tcp", List.of(PortBinding.of("", "5000")));
+
+        HostConfig hostConfig = HostConfig.builder()
+                .portBindings(stringListMap)
+                .build();
+
+        ContainerConfig.Builder config = ContainerConfig.builder()
+                .image(imageId)
+                .exposedPorts("5000/tcp")
+                .hostConfig(hostConfig)
+                .labels(Map.of("potapaas_deployment", potapaasAppId.substring(0, 13) + "..."));
+
+        return containerManager.runContainer(config);
+    }
+
+    private Either<String, String> buildImage(String appSourceDir) {
+        DockerImageManager.ImageType dockerImageType = Match(deploymentType).of(
+                Case($(DeploymentType.NODEJS), DockerImageManager.ImageType.NODEJS)
+        );
+
+        DockerImageManager imageManager = new DockerImageManager(PotapaasConfig.get("docker_api_uri"), appSourceDir, dockerImageType);
+        return imageManager.buildImage();
+    }
+
+    private Either<String, String> cloneRepo() {
+        Path tmpDir;
+        try {
+            tmpDir = Files.createTempDirectory("potapaas_tmp_git");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Either.left("create temp directory for github repo: " + e.getMessage());
+        }
+        GitCloner cloner = new GitCloner(tmpDir.toAbsolutePath().toString());
+        return cloner.cloneBranch(githubRepoUrl, branchName);
+    }
+}
---
+++ src/test/java/pl/potat0x/potapaas/potapaasservice/util/AppDeploymentTest.java
@@ -0,0 +1,25 @@
+package pl.potat0x.potapaas.potapaasservice.util;
+
+import io.vavr.control.Either;
+import org.junit.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppDeploymentTest {
+
+    @Test
+    public void shouldDeployWebAppFromGithub() throws InterruptedException {
+        AppDeployment app = new AppDeployment(AppDeployment.DeploymentType.NODEJS, "https://github.com/heroku/node-js-sample", "master");
+        Either<String, String> deploymentResult = app.deployFromGithub();
+
+        assertThat(deploymentResult.isRight()).isTrue();
+        Thread.sleep(1000);
+        ResponseEntity<String> response = new TestRestTemplate().getForEntity("http://127.0.0.1:5000/", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        app.killApp().get();
+    }
+}

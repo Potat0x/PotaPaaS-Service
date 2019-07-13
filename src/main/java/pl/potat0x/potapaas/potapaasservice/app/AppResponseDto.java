2a8dc6f41e5c0107433fde1123301587c71d3441
Potat0x <Potat0x@users.noreply.github.com>
2019-07-13 20:35:22 +0200

    Use Lombok to generate equals and hashCode

--- src/main/java/pl/potat0x/potapaas/potapaasservice/app/AppResponseDto.java
+++ src/main/java/pl/potat0x/potapaas/potapaasservice/app/AppResponseDto.java
@@ -2,11 +2,14 @@ package pl.potat0x.potapaas.potapaasservice.app;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
 
-public final class AppResponseDto {
+@ToString
+@EqualsAndHashCode
+final class AppResponseDto {
     private final String name;
     private final String type;
     private final String sourceRepoUrl;
@@ -65,39 +68,5 @@ public final class AppResponseDto {
 
     public int getExposedPort() {
         return exposedPort;
-    }
-
-    @Override
-    public String toString() {
-        return "AppResponseDto{" +
-                "name='" + name + '\'' +
-                ", type='" + type + '\'' +
-                ", sourceRepoUrl='" + sourceRepoUrl + '\'' +
-                ", sourceBranchName='" + sourceBranchName + '\'' +
-                ", createdAt=" + createdAt +
-                ", status='" + status + '\'' +
-                ", uptime='" + uptime + '\'' +
-                ", exposedPort=" + exposedPort +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        AppResponseDto that = (AppResponseDto) o;
-        return exposedPort == that.exposedPort &&
-                Objects.equals(name, that.name) &&
-                Objects.equals(type, that.type) &&
-                Objects.equals(sourceRepoUrl, that.sourceRepoUrl) &&
-                Objects.equals(sourceBranchName, that.sourceBranchName) &&
-                Objects.equals(createdAt, that.createdAt) &&
-                Objects.equals(status, that.status) &&
-                Objects.equals(uptime, that.uptime);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, type, sourceRepoUrl, sourceBranchName, createdAt, status, uptime, exposedPort);
     }
 }
